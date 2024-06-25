b122e21404e3eef530461dd6bffdea76283b6be0
Rachel Setyawan <rahelsetyawan321@gmail.com>
2024-06-25 11:46:14 +0700

    Deployment 1

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#7868E6',
+                black: '#121212',
+                grey: '#929292',
+                lightgrey: '#C7C9D9',
+            },
+            fontFamily: {
+                'inter': 'Inter',
+            }
+        },
+    },
+    plugins: [],
+}
